71d8ac3ce7b9b8c89a4014fdb52af25545855c3f
Filip Kinský <filip@filovo.net>
2012-12-25 22:50:32 +0100

    SampleWebFormsApp s rozchozenym holym springem

---
+++ SampleWebFormsApp/Pages/SamplePage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SamplePage.aspx.cs" Inherits="SampleWebFormsApp.Pages.SamplePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+	    <h1>Sample page</h1>
+		Injected text: <asp:Label runat="server" ID="lblInjectedText"></asp:Label>
+		Current time: <asp:Label runat="server" ID="lblCurrentTime"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SampleWebFormsApp/Pages/SamplePage.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SampleModule.Time;
+using Page = Spring.Web.UI.Page;
+
+namespace SampleWebFormsApp.Pages
+{
+	public partial class SamplePage : Page
+	{
+		public ISystemTime SystemTime { get; set; }
+
+		public string InjectedText { get; set; }
+
+		public string CurrentTime { get { return SystemTime.GetCurrentTime().ToString(); } }
+
+		public SamplePage()
+		{
+			InjectedText = "not injected yet";
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			lblInjectedText.Text = InjectedText;
+			lblCurrentTime.Text = CurrentTime;
+
+			base.OnLoad(e);
+		}
+	}
+}

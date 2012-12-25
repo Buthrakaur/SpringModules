2fb4a426b3217a532ebc80cb0a8effd2299b4644
Filip Kinský <filip@filovo.net>
2012-12-25 18:23:23 +0100

    LoggingExtensions.Warn

---
+++ SpringModules.Core/Logging/LoggingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace SpringModules.Core.Logging
+{
+	public static class LoggingExtensions
+	{
+		private static ILog GetLogger(Type type)
+		{
+			return LogManager.GetLogger(type);
+		}
+
+		public static void Warn(this object context, string message, Exception exception = null)
+		{
+			var log = GetLogger(context.GetType());
+			if (exception != null)
+			{
+				log.Warn(message, exception);
+			}
+			else
+			{
+				log.Warn(message);
+			}
+		}
+	}
+}

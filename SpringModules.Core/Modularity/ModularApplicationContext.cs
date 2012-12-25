9132b633d993f4ac1190af79af583bd6f8ba5805
Filip Kinský <filip@filovo.net>
2012-12-25 17:50:50 +0100

    ModularApplicationContext.Initialize() throw when called twice

--- SpringModules.Core/Modularity/ModularApplicationContext.cs
+++ SpringModules.Core/Modularity/ModularApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Spring.Context;
 using Spring.Context.Support;
 
@@ -15,6 +16,11 @@ namespace SpringModules.Core.Modularity
 
 		public void Initialize()
 		{
+			if (Container != null)
+			{
+				throw new InvalidOperationException("ModularApplicationContext is already initialized - it looks like you tried to call Initialize() twice.");
+			}
+
 			var container = new GenericApplicationContext();
 			var moduleInstaller = new ModuleInstaller(container);
 			modules.ForEach(m => m.Install(moduleInstaller));

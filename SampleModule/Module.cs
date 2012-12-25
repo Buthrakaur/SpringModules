995078a5b9573f4c0afa0605c1aa400718c9dd1e
Filip Kinský <filip@filovo.net>
2012-12-25 16:26:12 +0100

    rename/uklid

---
+++ SampleModule/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spring.Core.IO;
+using SpringModules.Core.Modularity;
+
+namespace SampleModule
+{
+	public class Module: IModule
+	{
+		public void Install(IModuleInstaller installer)
+		{
+			installer.RegisterXmlResource(new AssemblyResource("assembly:SampleModule/SampleModule.Time/objects.xml"));
+		}
+	}
+}

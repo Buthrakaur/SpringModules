8b750c26141e36a76c4d61286658b1b193755fe1
Filip Kinský <filip@filovo.net>
2012-12-27 22:18:36 +0100

    Person entity + NH mapping

---
+++ SampleModule.Nh/Domain/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleModule.Nh.Domain
+{
+	public class Person
+	{
+		public virtual long Id { get; private set; }
+		public virtual string Name { get; set; }
+
+		private Person()
+		{
+		}
+
+		public Person(string name)
+		{
+			Name = name;
+		}
+	}
+}
